22e6c69725f45b2fa18035b1a2263c0d150ded22
zhangqd <zhangqd@digiwin.com>
2025-01-07 09:32:15 +0800

    feat: 增加多语言i18n

---
+++ src/locales/index.js
@@ -0,0 +1,21 @@
+import { createI18n } from "vue-i18n";
+import en from './en.json'
+import zhCN from './zh-CN.json'
+
+const messages = {
+  zh: {
+    ...zhCN,
+  },
+  en: {
+    ...en,
+  },
+};
+
+const i18n = createI18n({
+  locale: "en", // 设置当前语言类型
+  legacy: false, // 如果要支持compositionAPI，此项必须设置为false;
+  globalInjection: true, // 全局注册$t方法
+  messages,
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
+import i18n from './locales'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(i18n)
 
 app.mount('#app')
