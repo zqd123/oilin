9cd312a77ace91b10527d1cf1685f3b7ce7f6475
zqd123 <9544850405@qq.com>
2025-01-11 22:23:44 +0800

    feat(package): 升级 @vueuse/core 至 12.4.0 版本并应用到项目中
    - 在 package.json 和 package-lock.json 中添加 @vueuse/core 依赖
    - 在 TodayOilPrices.vue 中使用 @vueuse/core 的 useDateFormat 和 useNow 函数
    - 更新 package-lock.json 中相关依赖的版本

--- src/views/home/TodayOilPrices.vue
+++ src/views/home/TodayOilPrices.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="today-box w-2/3 relative">
     <div class="prices-box flex flex-col items-center justify-center gap-3 pt-4">
-      <div class="date absolute -top-2 text-center">2025-01-09</div>
+      <div class="date absolute -top-2 text-center">{{ formatted }}</div>
       <div class="title text-base font-semibold">Today's oil prices（$/b）</div>
       <div class="prices w-full flex justify-evenly">
         <template v-for="item in todayOilPrices" :key="item.date">
@@ -20,6 +20,8 @@
 <script setup>
 import PricesValue from './PricesValue.vue'
 import { ref } from 'vue'
+import { useDateFormat, useNow } from '@vueuse/core'
+const formatted = useDateFormat(useNow(), 'YYYY-MM-DD')
 
 const todayOilPrices = ref([
   {
