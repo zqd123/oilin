224ed0a8eb10387dd65b4c97c41a4ac6b3fe2fca
zhangqd <zhangqd@digiwin.com>
2025-01-13 18:23:07 +0800

    feat(lange): 添加 JSON 文件格式化和处理功能
    - 新增 jsonFormate.js 文件，实现 JSON 文件读取、格式化和保存功能
    - 创建 output.json 文件，用于存储格式化后的 JSON 数据
    - 添加 zh-CN.json 文件，用于生成中文翻译内容
    - 包含一个示例 Excel 文件，用于导入数据并生成 JSON 文件

---
+++ lange/jsonFormate.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const path = require('path')
+
+function readJsonFileSync(filePath) {
+  try {
+    const fileContent = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8')
+    return JSON.parse(fileContent)
+  } catch (err) {
+    console.error('Error reading or parsing the JSON file:', err)
+  }
+}
+
+// 使用示例
+// const formateJson = (data) => {
+//   const result = []
+//   Object.keys(data).forEach((key) => {
+//     result.push({
+//       key,
+//       EnglishValue: data[key],
+//     })
+//   })
+//   return result
+// }
+
+async function saveFile(filePath, obj) {
+  try {
+    const jsonData = JSON.stringify(obj, null, 2) // 格式化输出
+    await fs.promises.writeFile(path.resolve(__dirname, filePath), jsonData)
+    console.log('File saved successfully.')
+  } catch (err) {
+    console.error('Error saving the file:', err)
+  }
+}
+
+//  导出JSON文件
+// const data = readJsonFileSync('../src/locales/en.json')
+// saveFile('./output.json', formateJson(data))
+
+function getExcelData(data) {
+  console.log('🚀 ~ getExcelData ~ data:', typeof data)
+  const result = {}
+  data.forEach((item) => {
+    result[item.key] = item.ChinaValue
+  })
+  return result
+}
+
+// 导入JSON
+const excelData = readJsonFileSync('./output.json')
+saveFile('./zh-CN.json', getExcelData(excelData))
